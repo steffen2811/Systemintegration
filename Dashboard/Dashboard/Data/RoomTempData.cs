d8f0cb838d451160ebebc0ba5120a02195ea11fc
Steffen K. Thomsen <steffen2811@live.dk>
2022-04-23 22:19:55 +0200

    Remove handling of timezoner (moved to blazor).

--- Dashboard/Dashboard/Data/RoomTempData.cs
+++ Dashboard/Dashboard/Data/RoomTempData.cs
@@ -5,18 +5,5 @@
         public decimal temperature { get; set; }
         public DateTime? updated { get; set; }
 
-        public RoomTempData(decimal temperature, DateTime? updated)
-        {
-            if (updated != null)
-            {
-                TimeZoneInfo cet = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
-                var utcTime = TimeZoneInfo.ConvertTimeToUtc((DateTime)updated, cet);
-
-                this.updated = utcTime.ToLocalTime();
-            }
-            else
-                this.updated = updated;
-            
-        }
     }
 }
